dc1ef5134332b4e6de698d28ca89375a16a5d0bb
hoon2hooni <sanghoon.engineer@gmail.com>
2022-12-19 12:07:10 +0900

    refac: remove unused tanstack query library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import { Global, ThemeProvider } from "@emotion/react";
 import global from "@styles/global";
 import theme from "@styles/theme";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
-import { useState } from "react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,23 +13,11 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const [queryClient] = useState(() => {
-    return new QueryClient({
-      defaultOptions: {
-        queries: {
-          refetchOnWindowFocus: false,
-        },
-      },
-    });
-  });
   const getLayout = Component.getLayout ?? ((page) => page);
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <Global styles={global} />
-        {getLayout(<Component {...pageProps} />)}
-      </ThemeProvider>
-      {/* <ReactQueryDevtools initialIsOpen={false} position={"top-left"} /> */}
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <Global styles={global} />
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
   );
 }
