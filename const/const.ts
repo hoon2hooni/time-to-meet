a3b96d475d01cbb1d90e88fe5127954d17f7bf0b
hoon2hooni <sanghoon.engineer@gmail.com>
2022-12-19 11:52:38 +0900

    refac: remove unused const file

--- const/const.ts
+++
@@ -1,6 +0,0 @@
-import type { InputProps } from "@customTypes";
-
-export const dateInputData: InputProps = [
-  { id: "startDate", size: "middle", unit: "부터", type: "date" },
-  { id: "endDate", size: "middle", unit: "까지", type: "date" },
-];
